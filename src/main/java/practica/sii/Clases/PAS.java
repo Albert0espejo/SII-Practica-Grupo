f815d85447d969cb5ad50a9fddf7f44904fecf37
Albert0espejo <albertowere12@outlook.com>
2020-03-29 17:41:28 +0200

    Usuario, PAS, PDI y Direccion.

---
+++ src/main/java/practica/sii/Clases/PAS.java
@@ -0,0 +1,63 @@
+package practica.sii.Clases;
+
+import java.io.Serializable;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: PAS
+ *
+ */
+@Entity
+
+public class PAS extends usuario implements Serializable {
+
+	
+	private String Facultad;
+	private String Servicio;
+	@Embedded
+	private Address Direccion;
+	private String Representante;
+	private String Telefono;
+	private static final long serialVersionUID = 1L;
+
+	public PAS() {
+		super();
+	}   
+	public String getFacultad() {
+		return this.Facultad;
+	}
+
+	public void setFacultad(String Facultad) {
+		this.Facultad = Facultad;
+	}   
+	public String getServicio() {
+		return this.Servicio;
+	}
+
+	public void setServicio(String Servicio) {
+		this.Servicio = Servicio;
+	}   
+	public Address getDireccion() {
+		return this.Direccion;
+	}
+
+	public void setDireccion(Address Direccion) {
+		this.Direccion = Direccion;
+	}   
+	public String getRepresentante() {
+		return this.Representante;
+	}
+
+	public void setRepresentante(String Representante) {
+		this.Representante = Representante;
+	}   
+	public String getTelefono() {
+		return this.Telefono;
+	}
+
+	public void setTelefono(String Telefono) {
+		this.Telefono = Telefono;
+	}
+   
+}
