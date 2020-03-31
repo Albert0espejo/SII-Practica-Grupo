7f0b6e0f7d1cfccc9960e67d81e64f5e44354378
hjkers <hjkers@hjkers>
2020-03-31 05:53:54 +0200

    Clase universidad creada

---
+++ src/main/java/practica/sii/Clases/Universidad.java
@@ -0,0 +1,66 @@
+package practica.sii.Clases;
+
+import java.io.Serializable;
+import java.lang.Integer;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+* Entity implementation class for Entity: usuario
+*
+*/
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Universidad implements Serializable{
+
+	
+	@Id_Universidad @GeneratedValue(strategy = GenerationType.AUTO)
+	private Long Id_Universidad;
+	private String Nombre;
+	private String Direccion;
+	private String Direccion_Web;
+	private String Ciudad;
+	private Long Codigo_Postal;
+	private static final long serialVersionUID = 1L;
+	
+	public Universidad() {
+		super();
+	}
+	public Long getId_Universidad() {
+		return this.Id_Universidad;
+	}
+	public void setId_Universidad(Long Id_Universidad) {
+		this.Id_Universidad = Id_Universidad;
+	}
+	public String getNombre() {
+		return this.Nombre;
+	}
+	public void setNombre(String Nombre) {
+		this.Nombre = Nombre;
+	}
+	public String getDireccion() {
+		return this.Nombre;
+	}
+	public void setDireccion(String Direccion) {
+		this.Direccion = Direccion;
+	}
+	public String getDireccion_Web() {
+		return this.Direccion_Web;
+	}
+	public void setDireccion_Web(String Direccion_Web) {
+		this.Direccion_Web = Direccion_Web;
+	}
+	public String getCiudad() {
+		return this.Ciudad;
+	}
+	public void setCiudad(String Ciudad) {
+		this.Ciudad = Ciudad;
+	}
+	public Long getCodigo_Postal() {
+		return this.Codigo_Postal;
+	}
+	public void setCodigo_Postal(Long Codigo_Postal) {
+		this.Codigo_Postal= Codigo_Postal;
+	}
+	
+}
