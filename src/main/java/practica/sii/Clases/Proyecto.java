8412fa773a4af50568dc2ee9184610e5bd736e7f
Carlos <Carlos@host.docker.internal>
2020-03-31 20:14:23 +0200

    .

---
+++ src/main/java/practica/sii/Clases/Proyecto.java
@@ -0,0 +1,119 @@
+package practica.sii.Clases;
+
+import java.io.Serializable;
+import java.lang.Long;
+import java.lang.String;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Proyecto
+ *
+ */
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Proyecto implements Serializable {
+
+	   
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	private Long ID_Proyecto;
+	private String Lugar;
+	@Temporal(TemporalType.DATE)
+	private Date Fecha_Inicio;
+	@Temporal(TemporalType.DATE)
+	private Date Fecha_Final;
+	private String Descripcion;
+	@Enumerated(EnumType.STRING)
+	private Estado Estado;
+	@OneToMany(mappedBy = "proyecto")
+	private List<Solicitud> solicitudes;
+	
+	public List<Solicitud> getSolicitudes() {
+		return solicitudes;
+	}
+	public void setSolicitudes(List<Solicitud> solicitudes) {
+		this.solicitudes = solicitudes;
+	}
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToMany
+	private List<ONG> ong;
+
+	public Proyecto() {
+		super();
+	}   
+	public Long getID_Proyecto() {
+		return this.ID_Proyecto;
+	}
+
+	public void setID_Proyecto(Long ID_Proyecto) {
+		this.ID_Proyecto = ID_Proyecto;
+	}   
+	public String getLugar() {
+		return this.Lugar;
+	}
+
+	public void setLugar(String Lugar) {
+		this.Lugar = Lugar;
+	}   
+	public Date getFecha_Inicio() {
+		return this.Fecha_Inicio;
+	}
+
+	public void setFecha_Inicio(Date Fecha_Inicio) {
+		this.Fecha_Inicio = Fecha_Inicio;
+	}   
+	public Date getFecha_Final() {
+		return this.Fecha_Final;
+	}
+
+	public void setFecha_Final(Date Fecha_Final) {
+		this.Fecha_Final = Fecha_Final;
+	}   
+	public String getDescripcion() {
+		return this.Descripcion;
+	}
+
+	public void setDescripcion(String Descripcion) {
+		this.Descripcion = Descripcion;
+	}   
+	public Estado getEstado() {
+		return this.Estado;
+	}
+
+	public void setEstado(Estado Estado) {
+		this.Estado = Estado;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ID_Proyecto == null) ? 0 : ID_Proyecto.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Proyecto other = (Proyecto) obj;
+		if (ID_Proyecto == null) {
+			if (other.ID_Proyecto != null)
+				return false;
+		} else if (!ID_Proyecto.equals(other.ID_Proyecto))
+			return false;
+		return true;
+	}
+	public List<ONG> getOng() {
+		return ong;
+	}
+	public void setOng(List<ONG> ong) {
+		this.ong = ong;
+	}
+}
---
+++ src/main/java/practica/sii/Clases/usuario.java
@@ -0,0 +1,87 @@
+package practica.sii.Clases;
+
+import java.io.Serializable;
+import java.lang.Integer;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: usuario
+ *
+ */
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+class usuario implements Serializable {
+
+	   
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	private Long Id;
+	private String usuario;
+	private String contrasena;
+	private String correo;
+	@ManyToOne
+	private Universidad universidad;
+	private static final long serialVersionUID = 1L;
+
+	public usuario() {
+		super();
+	}   
+	public Long getId() {
+		return this.Id;
+	}
+
+	public void setId(Long Id) {
+		this.Id = Id;
+	}   
+	public String getUsuario() {
+		return this.usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}   
+	public String getContrasena() {
+		return this.contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}   
+	public String getCorreo() {
+		return this.correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	
+	public Universidad getUniversidad() {
+		return universidad;
+	}
+	public void setUniversidad(Universidad universidad) {
+		this.universidad = universidad;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		usuario other = (usuario) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+}
