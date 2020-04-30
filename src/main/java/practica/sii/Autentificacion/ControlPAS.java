71b2d885828305a1a0ad4f7be02a920b01176abd
hjkers <hjkers@hjkers>
2020-04-30 13:25:25 +0200

    limpieza en controlPAS

--- src/main/java/practica/sii/Autentificacion/ControlPAS.java
+++ src/main/java/practica/sii/Autentificacion/ControlPAS.java
@@ -30,16 +30,10 @@ public class ControlPAS implements Serializable{
 	private static final long serialVersionUID = 1L; 
 	private List<Usuario> listaUsuarios;
 	private List<Solicitud> listaSolicitudes;
-	private List<Universidad> listaUniversidades;
-	private List<Noticias> listaNoticias;
-	private List<Proyecto> listaProyectos;
 	private List<Usuario> listaProfesores;
 	
 	private Boolean showtableUsuarios = false;
 	private Boolean showtableSolicitudes = false;
-	private Boolean showtableUniversidades = false;
-	private Boolean showtableNoticias = false;
-	private Boolean showtableProyectos = false;
 	
 	private Login listas;
 	private Usuario miusuario;
@@ -49,10 +43,7 @@ public class ControlPAS implements Serializable{
 		
 		listaProfesores = new ArrayList<Usuario>();
 		listaUsuarios = new ArrayList<Usuario>();
-		listaNoticias = new ArrayList<Noticias>();
-		listaProyectos = new ArrayList<Proyecto>();
 		listaSolicitudes = new ArrayList<Solicitud>();
-		listaUniversidades = new ArrayList<Universidad>();
 	}
 	
 	public Usuario getMiusuario() {
@@ -78,9 +69,6 @@ public class ControlPAS implements Serializable{
 		return listaSolicitudes;
 	}
 
-	public List<Universidad> getListaUniversidades() {
-		return listaUniversidades;
-	}
 	
 	public List<Usuario> getListaProfesores(){
 		List<Usuario> aux = listas.getUsuarios();
@@ -92,13 +80,6 @@ public class ControlPAS implements Serializable{
 		return listaProfesores; //prueba
 	}
 
-	public List<Noticias> getListaNoticias() {
-		return listaNoticias;
-	}
-
-	public List<Proyecto> getListaProyectos() {
-		return listaProyectos;
-	}
 	
 	public Boolean getShowtableUsuarios() {
 		return showtableUsuarios;
@@ -107,73 +88,15 @@ public class ControlPAS implements Serializable{
 	public Boolean getShowtableSolicitudes() {
 		return showtableSolicitudes;
 	}
-
-	public Boolean getShowtableUniversidades() {
-		return showtableUniversidades;
-	}
-
-	public Boolean getShowtableNoticias() {
-		return showtableNoticias;
-	}
-
-	public Boolean getShowtableProyectos() {
-		return showtableProyectos;
-	}
-
-	public void enabletableUsuarios() {
-		if(showtableNoticias || showtableSolicitudes || showtableProyectos || showtableUniversidades) {
-			showtableNoticias = false;
-			showtableSolicitudes = false;
-			showtableProyectos = false;
-			showtableUniversidades = false;
-		}
-
-		showtableUsuarios = !showtableUsuarios;
-	}
 	
 	public void enabletableSolicitudes() {
-		if(showtableNoticias || showtableUsuarios || showtableProyectos || showtableUniversidades) {
-			showtableNoticias = false;
+		if(showtableUsuarios) {
 			showtableUsuarios = false;
-			showtableProyectos = false;
-			showtableUniversidades = false;
 		}
 		
 		showtableSolicitudes = !showtableSolicitudes;
 	}
 	
-	public void enabletableUniversidades() {
-		if(showtableNoticias || showtableUsuarios || showtableProyectos || showtableSolicitudes) {
-			showtableNoticias = false;
-			showtableUsuarios = false;
-			showtableProyectos = false;
-			showtableSolicitudes = false;
-		}
-		
-		showtableUniversidades = !showtableUniversidades;
-	}
-	
-	public void enabletableNoticias() {
-		if(showtableUniversidades || showtableUsuarios || showtableProyectos || showtableSolicitudes) {
-			showtableUniversidades = false;
-			showtableUsuarios = false;
-			showtableProyectos = false;
-			showtableSolicitudes = false;
-		}
-		
-		showtableNoticias = !showtableNoticias;
-	}
-	
-	public void enabletableProyectos() {
-		if(showtableNoticias || showtableUsuarios || showtableUniversidades || showtableSolicitudes) {
-			showtableNoticias = false;
-			showtableUsuarios = false;
-			showtableUniversidades = false;
-			showtableSolicitudes = false;
-		}
-		
-		showtableProyectos = !showtableProyectos;
-	}
 	
 	public void borrarSolicitudes(Solicitud s) {
 		listaSolicitudes.remove(s);
